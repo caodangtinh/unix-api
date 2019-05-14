56607ec6cfa07d3a37115b4e59d170af15beecba
tinh cao <caodangtinh@gmail.com>
2019-05-14 11:04:32 +0800

    add UnitTest

--- src/test/java/com/rupinder/controller/UnixGroupControllerTest.java
+++ src/test/java/com/rupinder/controller/UnixGroupControllerTest.java
@@ -1,6 +1,7 @@
 package com.rupinder.controller;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -24,7 +25,8 @@ import com.rupinder.service.UnixService;
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
-public class UnixUserControllerTest {
+public class UnixGroupControllerTest {
+
 	private static final PwdUser PWD_USER = new PwdUser("tinhcao:x:1000:1000:tinhcao,,,:/home/tinhcao:/bin/bash");
 	private static final Group GROUP = new Group("tinhcao:x:1000:");
 
@@ -35,41 +37,24 @@ public class UnixUserControllerTest {
 	private UnixService unixService;
 
 	@Test
-	public void testGetListPwdUser() throws Exception {
-		// mock call
-		when(unixService.getListPwdUser()).thenReturn(Collections.singletonList(PWD_USER));
-		//
-		mockMvc.perform(get("/api/v1/users")).andExpect(status().isOk())
+	public void testGetListGroup() throws Exception {
+		when(unixService.getListGroup()).thenReturn(Collections.singletonList(GROUP));
+		// 
+		mockMvc.perform(get("api/v1/groups"))
+		.andExpect(status().isOk())
 				.andExpect(content().string(containsString("name")));
 	}
 
 	@Test
-	public void testGetListPwdUserById() throws Exception {
-		// mock call
-		when(unixService.getPwdUserBbyId(1)).thenReturn(PWD_USER);
-		//
-		mockMvc.perform(get("/api/v1/users/1")).andExpect(status().isOk())
-				.andExpect(content().string(containsString("tinhcao")));
-	}
-
-	@Test
-	public void testGetListGroupOfUser() throws Exception {
-		// mock call
-		when(unixService.getPwdUserBbyId(1)).thenReturn(PWD_USER);
+	public void testGetPwdGroupBbyId() throws Exception {
 		when(unixService.getPwdGroupBbyId(1000)).thenReturn(GROUP);
-		//
-		mockMvc.perform(get("/api/v1/users/1/groups")).andExpect(status().isOk())
-				.andExpect(content().string(containsString("tinhcao")));
+		mockMvc.perform(get("api/v1/groups/1000")).andExpect(status().isOk())
+				.andExpect(content().string(containsString("name")));
 	}
 
 	@Test
-	public void testFilterUser() throws Exception {
-		// mock call
-		when(unixService.filterPwdUser("tinhcao", null, null, null, null, null))
-				.thenReturn(Collections.singletonList(PWD_USER));
-		//
-		mockMvc.perform(get("/api/v1/users/query?name=tinhcao")).andExpect(status().isOk())
-				.andExpect(content().string(containsString("tinhcao")));
+	public void testFilterGroup() {
+		fail("Not yet implemented");
 	}
 
 }
